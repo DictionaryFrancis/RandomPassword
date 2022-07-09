8ea46e2ca8fe7bc71767da700fa055ff723a1e4f
Francis Matias <fwmatias64@gmail.com>
2022-07-09 13:31:07 +0100

    JavaScript and HTML changes
    I`m making some changes in the js which was refer to a two different function names

--- PasswordGenerator/index.js
+++ PasswordGenerator/index.js
@@ -1,11 +1,23 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-const passwordOne = document.getElementById("password-one")
-const passwordTwo = document.getElementById("password-two")
-const generateButton = document.getElementById("generate-btn")
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
 
-generateButton.addEventListener("click", function insertPassword(characters){
-    return characters[Math.floor(Math.random().toString(8) * characters.length)]
-})
-console.log(insertPassword[characters])
+function getRandomPassword(){
+    let password = ""
+    let passwordLenght = 10
+        for(let i =0;i < passwordLenght; i++){
+            let randomIndex = Math.floor(Math.random() * characters.length)
+            password += characters[randomIndex]
+        }
+        return password
+}
+
+function getPasswords(){
+    let firstPassword = getRandomPassword()
+    let secondPassword = getRandomPassword()
+    passwordOne.textContent = firstPassword
+    passwordTwo.textContent = secondPassword
+}
+
